a4bbbc7c7b91251baf1e714f2225994669366065
SolarPal <tumaf@mail.ru>
2023-07-14 22:26:42 +0300

    feat: implement the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split("");
+  
+  for (let i = 0; i < strArr.length; i += 1) {
+    //console.log("======================")
+    //console.log("i=" + i);
+    for (let iConf = 0; iConf < bracketsConfig.length; iConf += 1) {
+      if (strArr[i] === bracketsConfig[iConf][0] && strArr[i + 1] === bracketsConfig[iConf][1]) {
+        //console.log("Brackets: " + bracketsConfig[iConf][0] + bracketsConfig[iConf][1] + " REMOVED! at i=" + i + " in " + strArr);
+        strArr.splice(i, 2);
+        //console.log("New array: " + strArr);
+        i = -1;
+        iConf = -1;
+      } else {
+        continue;
+      }
+    }
+  }
+  
+  //console.log(strArr);
+  
+  return strArr.length === 0;
 }
